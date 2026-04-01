7b591cd52cd8b7abf302b344a9c7373ab92e9104
Vincent Mutwiri <vincentmutwiri9@gmail.com>
2026-04-01 20:51:53 +0300

    Refactor PSBT output command
    Remove unnecessary echo command for PSBT.

--- submission/05.sh
+++ submission/05.sh
@@ -1,4 +1,3 @@
-#!/usr/bin/env bash
 set -euo pipefail
 
 # Create a partially signed transaction from the details below.
@@ -6,4 +5,3 @@ set -euo pipefail
 PSBT='cHNidP8BAHwCAAAAAhYO5d0UYxa7NADt4NStUSq5we3khqtaEZou6dQ3n8EjAAAAAAD9////Fg7l3RRjFrs0AO3g1K1RKrnB7eSGq1oRmi7p1DefwSMBAAAAAP3///8BAC0xAQAAAAAXqRQh7ZB2LhbqrqGIquGRQuWyW/ddI4cAAAAAAAAAAA=='
 
 echo "$PSBT"
-
